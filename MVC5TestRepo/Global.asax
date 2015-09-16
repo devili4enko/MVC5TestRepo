b2ec5bcb6654681426799987508307fefad4202a
oleg.dekhtiarenko <oleg.dekhtiarenko@wonga.com>
2015-09-16 15:38:48 +0300

    Init

---
+++ MVC5TestRepo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5TestRepo.MvcApplication" Language="C#" %>
---
+++ MVC5TestRepo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5TestRepo.Startup))]
+namespace MVC5TestRepo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
