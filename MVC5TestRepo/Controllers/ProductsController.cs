3ef76451639c94e537d4e9aecbec44b4cca4657a
oleg.dekhtiarenko <devili4enko@gmail.com>
2015-09-21 22:54:08 +0300

    Add web api controller

---
+++ MVC5TestRepo/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Models.SportStoreModels;
+using MVC5TestRepo.Repositories.Implementation;
+using MVC5TestRepo.Repositories.Interfaces;
+
+namespace MVC5TestRepo.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private readonly ISportStoreRepository _repository;
+
+        public ProductsController()
+        {
+            _repository = new SportStoreRepository();
+        }
+
+        public IEnumerable<Product> Get()
+        {
+            return _repository.Products;
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _repository.Products.Where(p => p.Id == id).FirstOrDefault();
+        }
+        public async Task<int> PostProduct(Product product)
+        {
+            return await _repository.SaveProductAsync(product);
+        }
+
+        public async Task<Product> DeleteProduct(int id)
+        {
+            return await _repository.DeleteProductAsync(id);
+        }
+
+        [Route("api/Products/Delete/")]
+        [HttpPost]
+        public async Task<Product> PostDeleteProduct(Product product)
+        {
+            return await _repository.DeleteProductAsync(product.Id);
+        }
+
+    }
+}
